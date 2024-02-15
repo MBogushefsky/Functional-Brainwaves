4c9d99b25c6486cca889959c79c03d3cda0e2d19
MBogushefsky <mbogushefsky@gmail.com>
2024-02-15 15:19:33 -0700

    Adjust post clone script to resolve build

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+HOMEBREW_NO_INSTALL_CLEANUP=TRUE # disable homebrew's cleanup.
 HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install node
 brew install cocoapods
