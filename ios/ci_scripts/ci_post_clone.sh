4f1fb556b1a9b1f7c2a10801d50bfacfaf21d2fd
MBogushefsky <mbogushefsky@gmail.com>
2024-02-15 15:59:32 -0700

    Cleanup

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-HOMEBREW_NO_INSTALL_CLEANUP=1 # disable homebrew's cleanup.
+HOMEBREW_NO_INSTALL_CLEANUP=TRUE # disable homebrew's cleanup.
 HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install node
 brew install cocoapods
